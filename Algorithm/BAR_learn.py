145f61da8d14775ddd709c766c1cf20c0536d5e8
felixschreyer <felixschreyer@mailbox.org>
2023-11-08 15:37:56 +0100

    Multiprocessing & Grid Search

--- Algorithm/BAR_learn.py
+++ Algorithm/BAR_learn.py
@@ -1,27 +1,11 @@
-import pickle
-
-import pandas as pd
-from matplotlib import pyplot as plt
+import pandas
 from sklearn.preprocessing import MinMaxScaler
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from utils import *
 
-# Load Model
-model = joblib.load('../Notebooks/classification_model.joblib')
-# Load Data
-X = pd.read_pickle('../Data.nosync+/X_t_train.pkl')
-y = pd.read_pickle('../Data.nosync+/y_t_train.pkl')
-X_s = pd.read_parquet(path='../Notebooks/turbofan_features.parquet', engine='pyarrow')
-# Define Parameters
-learning_rate = 0.1
-epochs = 100
-q = 20
-beta = 0.01
-bias = 1
 
-
-def one_sided_averaged_gradient_estimator(W, q, beta, bias, X, y, iteration):
+def one_sided_averaged_gradient_estimator(W, q, beta, bias, X, y, iteration, learning_rate, punishment, num_processes):
     """
     Creates Updated Weights based on Zeroth Order-Optimisation.
     Calculates the gradient of W in q different directions based on a loss function
@@ -45,93 +29,93 @@ def one_sided_averaged_gradient_estimator(W, q, beta, bias, X, y, iteration):
     """
 
     gradient_estimates = []
+
     for _ in range(q):
         U_j = get_U(W)
 
-        loss_plus = loss_funct(W=W + beta * U_j, X=X, y=y)
-        loss_minus = loss_funct(W=W, X=X, y=y)
+        loss_plus = loss_funct(W=W + beta * U_j, X=X, y=y, punishment=punishment)
+        loss_minus = loss_funct(W=W, X=X, y=y, punishment=punishment)
         g_j = bias / beta * (loss_minus - loss_plus) * U_j
 
         gradient_estimates.append(g_j)
-
     averaged_gradient = np.mean(gradient_estimates, axis=0)
 
     W_updated = update_W(W=W, alpha=learning_rate, gradient=averaged_gradient, iteration=iteration)
-
     return W_updated
 
 
-if __name__ == '__main__':
-
+def data_setup(model=joblib.load('../Notebooks/classification_model.joblib'),
+               X=pd.read_pickle('../Data/X_t_train.pkl'),
+               y=pd.read_pickle('../Data/y_t_train.pkl'),
+               X_s=pd.read_parquet(path='../Notebooks/turbofan_features.parquet', engine='pyarrow'),
+               learning_rate=0.4, epochs=20, q=20, beta=1, bias=1, n_splits=5, punishment=0.5, num_processes=5):
     loss_hist = []
     weights = []
 
-    # Prep X by renaming columns and scaling data
-    X = rename_columns(X_s, X)
+    # Rename Columns to Target Column Names
+    X = rename_columns(target=X_s, source=X)
+
+    # Scale Dataframe
     scaler = MinMaxScaler()
     X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
-    # Create Random Weights
+    # Create Weights
     num_rows, num_cols = X.shape
-    W = np.random.rand(1, num_cols)
+    #W = np.random.rand(1, num_cols)
     W = np.ones((1, num_cols))
+    #W = np.full(num_cols, -2)
+
+    #W = pd.read_pickle('../Notebooks/weights_3.pkl')
     W_0 = W
-    #W = pd.read_pickle('../Algorithm/weights_2.pkl')
 
     # Check initial performance
-    print("Initial performance is: ", loss_funct(W, X, y))
-
-    n_splits = 5
+    initial_perf = loss_funct(W, X, y, punishment=punishment)
+    print("Initial performance is: ", initial_perf)
+    loss_hist.append(initial_perf)
+    weights.append(W)
 
     oversampler = RandomOverSampler(sampling_strategy='auto')
     undersampler = RandomUnderSampler(sampling_strategy='auto')
 
+
     for config in range(n_splits):
         if config % 2 == 1:
-            X_train_resampled, y_train_resampled = undersampler.fit_resample(X, y)
+            X_train_resampled, y_train_resampled = oversampler.fit_resample(X, y)
         else:
             X_train_resampled, y_train_resampled = undersampler.fit_resample(X, y)
+
 
         # Update weights epoch times
         for _ in range(epochs):
             W = one_sided_averaged_gradient_estimator(W=W, q=q, beta=beta, bias=bias, X=X_train_resampled,
-                                                      y=y_train_resampled, iteration=epochs)
-            loss_hist.append(loss_funct(W, X * W, y))
+                                                      y=y_train_resampled, iteration=_,
+                                                      learning_rate=learning_rate, punishment=punishment, num_processes=num_processes)
+            loss_hist.append(loss_funct(W, X, y, punishment=punishment))
             weights.append(W)
 
         print_progress_bar(config + 1, n_splits)
 
     # Check final performance
-    print("best W is ", weights[loss_hist.index(min(loss_hist))])
-    print("Final performance is: ", min(loss_hist))
+    print("Best W is ", weights[loss_hist.index(min(loss_hist))])
+    print("Best performance is: ", min(loss_hist))
+    print("Corresponding performance is ", loss_funct(weights[loss_hist.index(min(loss_hist))], X, y, 2))
 
     # Print performance over iterations
-    plt.bar(range(len(loss_hist)), loss_hist)
+    plot_loss(loss_hist=loss_hist)
 
-    # Add labels and a title
-    plt.xlabel('Data Points')
-    plt.ylabel('Values')
-    plt.title('Bar Plot of Floats')
+    store_weights(file_path="weights.pkl", weights=weights[loss_hist.index(min(loss_hist))], loss=min(loss_hist), W=W, W_0=W_0)
 
-    # Show the plot
-    plt.show()
-
-    file_path = "weights_2.pkl"
-
-    # Save the list as a binary file
-    with open(file_path, "wb") as file:
-        pickle.dump(weights[loss_hist.index(min(loss_hist))], file)
-
-    print("List saved to", file_path)
-
-    df = pd.DataFrame(W-W_0)
-    def color_negative_red(val):
-        color = 'red' if val < 0.5 else 'green'  # Define your color conditions
-        return f'color: {color}'
+    result = calculate_f1_score(W, X, y)
+    return result
 
 
-    # Apply the custom styling function to the dataframe
-    styled_df = df.style.applymap(color_negative_red)
 
-    # Display the styled dataframe
-    df.to_csv('output.csv', index=False)
+
+if __name__ == '__main__':
+    num_processes = multiprocessing.cpu_count()
+
+    result = data_setup(model=joblib.load('../Notebooks/classification_model.joblib'),
+               X=pd.read_pickle('../Data/X_t_train.pkl'),
+               y=pd.read_pickle('../Data/y_t_train.pkl'),
+               X_s=pd.read_parquet(path='../Notebooks/turbofan_features.parquet', engine='pyarrow'),
+               learning_rate=0.01, epochs=500, q=15, beta=1, bias=1, n_splits=1, punishment=2, num_processes=1)
--- Algorithm/utils.py
+++ Algorithm/utils.py
@@ -1,43 +1,95 @@
 import math
+import multiprocessing
+import pickle
 
 import numpy as np
+import openpyxl
 import pandas as pd
 import joblib
+from matplotlib import pyplot as plt
 from sklearn.metrics import log_loss, f1_score, accuracy_score, recall_score, precision_score, confusion_matrix
+from sklearn.utils import compute_class_weight
 
+lock = multiprocessing.Lock()
 
 def transfer_X(X, W):
+    '''
+    Takes Feature Vector and Weight Vector. Returns Vector of Features multiplied with their weight.
+    :param X: Feature Vector
+    :type X: pandas.DataFrame
+    :param W: Weight Vector
+    :type W: numpy.ndarray
+    :return: Weighted Feature Vector
+    :rtype: pandas.DataFrame
+    '''
     X_hat = X * W
     return X_hat
 
 
-def loss_funct(W, X, y):
+def loss_funct(W, X, y, punishment):
     y_true = y
     sample_weights = {
-        1: 1.66667,
-        2: 3.5,
-        3: 0.53846154
+        # 1: 1.66667,
+        # 2: 3.5,
+        # 3: 0.53846154
+        1: 1.75,
+        2: 2.333333,
+        3: 0.5
     }
+    sample_weights = compute_class_weight('balanced', classes=np.unique(y), y=y)
+
     X_hat = transfer_X(X, W)
     model = load_model()
     output = model.predict_proba(X_hat)
     output_translated = map_output(output)
-    loss = log_loss(y, output_translated, labels=np.array([0, 1, 2]), sample_weight=[sample_weights[y] for y in y_true])
+    loss = log_loss(y, output_translated, labels=np.array([0, 1, 2]), sample_weight=[sample_weights[y-1] for y in y_true])
 
     class_of_interest = 1
     predictions = np.argmax(output_translated, axis=1)
-    loss_p = precision_score(y_true, predictions, labels = [class_of_interest], #labels=np.unique(predictions),  # labels=np.array([0, 1, 2]),
-                             average='micro', zero_division=0)
+    # loss_p = precision_score(y_true, predictions, labels = [class_of_interest], #labels=np.unique(predictions),  # labels=np.array([0, 1, 2]),
+    #                         average='micro', zero_division=0)
 
-    return loss #+ (1 - 0.1 * loss_p)
+    # y_diff = output_translated - y_true
+    # y_diff = output_translated
+    true_labels_array = y_true.to_numpy()
+    predicted_probabilities = output_translated[np.arange(len(output_translated)), true_labels_array - 1]
+    differences = np.where(predicted_probabilities > 0.6, 0, np.abs(0.6 - predicted_probabilities))
+    differences_sum = differences.sum()
+
+    predicted_classes = np.argmax(output_translated, axis=1)
+    predicted_probabilities = output_translated[np.arange(len(output_translated)), true_labels_array - 1]
+    predicted_classes = output_translated[np.arange(len(output_translated)), predicted_classes]
+    differences = predicted_classes - predicted_probabilities
+
+    # differences[differences < 0.1] = 0.1
+
+    false_classifications = np.sum(y_true != predictions)
+
+    differences_sum = differences.sum()
+
+    return loss + punishment * differences_sum
 
 
 def load_model():
+    '''
+    Loads Model of Random Forest
+    :return: sklearn model
+    :rtype: sklearn.model
+    '''
     model = joblib.load('../Notebooks/classification_model.joblib')
     return model
 
 
 def map_output(y):
+    '''
+    Maps output classes from source domain to classes from target domain.
+    Maps second and third class of source to second class of target domain.
+    See BAR paper for further explanations.
+    :param y: predicted classes
+    :type y: pandas.DataFrame
+    :return: predicted classes mapped to target domain
+    :rtype: pandas.DataFrame
+    '''
     y[:, 2] += y[:, 3]
     y = np.delete(y, 3, axis=1)
     y_norm = y / np.sum(y, axis=1)[:, np.newaxis]
@@ -45,8 +97,12 @@ def map_output(y):
 
 
 def get_U(W):
-    # Generate Random Vector U
-    # U_j ∈ R_d is a vector that is uniformly drawn at random from a unit Euclidean sphere
+    '''
+    Generates a vector uniformly drawn at random from a unit euclidean sphere of the dimension of W.
+    :param W: Weight Vector
+    :return: random vector of dimension dim(W)
+    :rtype: numpy.ndarray
+    '''
     U_j = np.random.normal(size=W.shape)
     # U_j /= np.linalg.norm(U_j)
 
@@ -54,50 +110,57 @@ def get_U(W):
 
 
 def update_W(W, alpha, gradient, iteration):
+    '''
+    Updates W either linearly or following an e-Function.
+    :param W: Old Weight Vector
+    :type W: pandas.DataFrame
+    :param alpha: learning rate
+    :type alpha: float
+    :param gradient: estimated gradient at W
+    :type gradient: np.ndarray
+    :param iteration: Iteration in optimization process for update via e-Function
+    :type iteration: int
+    :return: new Weights
+    :rtype: pandas.DataFrame
+    '''
+
     # linear
     W = W + alpha * gradient
-    # e-Function
-    #decay = 0.005
-    #W = W + math.exp(-decay*iteration) * alpha * gradient
 
+    # e-Function
+    #print(iteration)
+    #iteration+=1
+    #decay = 0.02
+    #W = W + math.exp(-decay * iteration) * alpha * gradient
+    #print(np.max(math.exp(-decay * iteration) * alpha * gradient))
     return W
 
 
-def load_small_X():
-    X = pd.read_parquet('../Notebooks/turbofan_features.parquet', engine='pyarrow')
-    return X.iloc[:2000]
+def rename_columns(target, source):
+    '''
+    Converts the column names aka feature names from those of the target domain to those of the source domain.
+    If the number of columns in the target domain is smaller than in the source domain, columns with value 0 are added.
+    If the target domain has more columns, a problem arises, the target domain columns need to be reduced then.
+    :param target: Dataframe of Target Domain Data
+    :type target: pandas.DataFrame
+    :param source: Dataframe of Source Domain Data
+    :type source: pandas.DataFrame
+    :return: Dataframe that contains target data in format of source data
+    :rtype: pandas.DataFrame
+    '''
+    column_names_target = target.columns.tolist()
+    column_names_source = source.columns.tolist()
 
+    extra_columns = list(set(column_names_target) - set(column_names_source))
 
-def load_small_dataset(sample_size):
-    y = pd.read_parquet('../Notebooks/turbofan_RUL.parquet', engine='pyarrow')
-    X = pd.read_parquet('../Notebooks/turbofan_features.parquet', engine='pyarrow')
-    # y = y.sample(sample_size)
-    # X = X.sample(sample_size)
-    y = y.values.T
-    flat_list = [item for sublist in y for item in sublist]
-    flat_list = np.array(flat_list)
-    return flat_list, X
-
-
-def load_small_transfer_dataset(sample_size):
-    y = pd.read_parquet('../Notebooks/turbofan_RUL.parquet', engine='pyarrow')
-    X = pd.read_parquet('../Notebooks/turbofan_RUL.parquet', engine='pyarrow')
-
-
-def rename_columns(df_A, df_B):
-    column_names_A = df_A.columns.tolist()
-    column_names_B = df_B.columns.tolist()
-
-    extra_columns = list(set(column_names_A) - set(column_names_B))
-
-    df_B.columns = column_names_A[:len(column_names_B)]
+    source.columns = column_names_target[:len(column_names_source)]
 
     for column_name in extra_columns:
-        if column_name not in df_B.columns:
-            df_B[column_name] = 0
+        if column_name not in source.columns:
+            source[column_name] = 0
 
-    df_B = df_B[df_A.columns]
-    return df_B
+    df_final = source[target.columns]
+    return df_final
 
 
 def print_progress_bar(iteration, total, length=50):
@@ -105,3 +168,66 @@ def print_progress_bar(iteration, total, length=50):
     arrow = '=' * int(length * progress)
     spaces = ' ' * (length - len(arrow))
     print(f'\r[{arrow}{spaces}] {int(progress * 100)}% \n', end='', flush=True)
+
+
+def plot_loss(loss_hist):
+    plt.bar(range(len(loss_hist)), loss_hist)
+
+    # Add labels and a title
+    plt.xlabel('Data Points')
+    plt.ylabel('Values')
+    plt.title('Bar Plot of Floats')
+
+    # Show the plot
+    plt.show()
+
+
+def store_weights(file_path, weights, loss, W, W_0):
+    # Save the list as a binary file
+    with open(file_path, "wb") as file:
+        pickle.dump(weights, file)
+
+    np.save("weights", weights)
+
+    print("List saved to", file_path)
+    df = pd.DataFrame(W - W_0)
+    df.to_csv('output.csv', index=False)
+
+
+def calculate_f1_score(W, X, y):
+    y_true = y
+    sample_weights = {
+        # 1: 1.66667,
+        # 2: 3.5,
+        # 3: 0.53846154
+        1: 1.75,
+        2: 2.333333,
+        3: 0.5
+    }
+    X_hat = transfer_X(X, W)
+    model = load_model()
+    output = model.predict_proba(X_hat)
+    output_translated = map_output(output)
+    predictions = np.argmax(output_translated, axis=1)
+    result = f1_score(y_true=y_true, y_pred=predictions + 1, average='macro')
+    false_classifications = np.sum(y_true != predictions + 1)
+    return result
+
+
+def write_to_excel(settings, path):
+    keys_to_exclude = ['model', 'X', 'y', 'X_s']
+    wb = openpyxl.load_workbook(path)
+    sheet = wb['Sheet3']
+    next_row = sheet.max_row + 1
+
+    for setting in settings:
+        filtered_params = {key: value for key, value in setting.items() if key not in keys_to_exclude}
+
+        new_data = list(filtered_params.values())
+
+        for col, value in enumerate(new_data, start=1):
+            sheet.cell(row=next_row, column=col, value=value)
+
+        next_row += 1
+
+    wb.save(path)
